d063f36f3ed794db7e6a55c2e481309ff29864d2
Flávio Neto <flavioneto96@gmail.com>
2021-11-08 19:47:42 -0300

    redesign

--- src/containers/Home/MeetTheTeam/index.js
+++ src/containers/Home/MeetTheTeam/index.js
@@ -1,3 +1,5 @@
+import BlockContent from '@sanity/block-content-to-react';
+
 import Section from '../../../components/Section';
 import { urlFor } from '../../../sanity/imageUrl';
 
@@ -11,16 +13,25 @@ export default function MeetTheTeam({ data }) {
     <Section addClasses={[styles.team]}>
       <div className={utilStyles.container}>
         <Section.Title left>{teamTitle}</Section.Title>
-        <div className={styles.team__grid}>
-          {employees.map(({ _key, name, position, photo }) => (
-            <div className={styles.team__employee} key={_key}>
-              <img src={urlFor(photo).width(600).height(400).url()} alt="Foto do funcionário"
-                className={styles.employee__picture}
-              />
-              <h2 className={styles.employee__name}>{name}</h2>
-              <p className={styles.employee__function}>{position}</p>
+        <div className={styles.divisor}>
+          <div className={styles.divisor__right}>
+            <Section.Paragraph>
+              <BlockContent blocks={teamText} />
+            </Section.Paragraph>
+          </div>
+          <div className={styles.divisor__left}>
+            <div className={styles.team__grid}>
+              {employees.map(({ _key, name, position, photo }) => (
+                <div className={styles.team__employee} key={_key}>
+                  <img src={urlFor(photo).width(600).height(400).url()} alt="Foto do funcionário"
+                    className={styles.employee__picture}
+                  />
+                  <h2 className={styles.employee__name}>{name}</h2>
+                  <p className={styles.employee__function}>{position}</p>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </Section>
